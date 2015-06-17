5fdce8e66253911115f638b2d84c6cb0748588a7
Torsten Kockler <torsten.kockler@uni-tuebingen.de>
2015-06-17 07:19:01 +0200

    Tuebix Puppet

---
+++ js/puppet.js
@@ -0,0 +1,11 @@
+function unloadVideo(videoSlot) {
+  var video = document.getElementById(videoSlot);
+  video.pause();
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
